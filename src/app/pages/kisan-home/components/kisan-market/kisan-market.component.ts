00e49127d752be76f7422f7cae5c8f9a7cdfb359
achaud53 <chaudhariaditya16@gmail.com>
2024-05-21 20:19:29 +0530

    KisanAssist API Integration
    KisanAssist API Integration

--- src/app/pages/kisan-home/components/kisan-market/kisan-market.component.ts
+++ src/app/pages/kisan-home/components/kisan-market/kisan-market.component.ts
@@ -1,48 +1,90 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Product } from 'src/app/models/product.model';
+import { StoreService } from 'src/app/services/store.service';
 
 @Component({
   selector: 'app-kisan-market',
   templateUrl: './kisan-market.component.html',
   styleUrls: ['./kisan-market.component.css']
 })
+
+
 export class KisanMarketComponent implements OnInit {
 
   @Input() categoryType : string | undefined;
   @Input() product: Product | undefined;
   @Output() addToCart = new EventEmitter();
+  products: Array<Product> | undefined;
+  productSubscription: Subscription | undefined;
+  sort: string = 'desc';
+  count: string = '12';
   // isElectricS : boolean = false;
   // isMedicalS : boolean = false;
   // isIrrigationS : boolean = false;
   // isConstructionAS: boolean = false;
 
-  products: Product[] = [
+  products2: Product[] = [
     {
       id: 1,
-      title: 'Model Cotton Seed Kaveri ATM BG-2',
+      name: 'Model Cotton Seed Kaveri ATM BG-2',
+      brand:"XYZ",
+      availability:"In Stock",
       price: 10,
       category: 'Category 1',
       description: 'Description for Product 1',
-      image: 'product1.jpg'
+      imageUrl: 'product1.jpg'
     },
     {
       id: 2,
-      title: 'Product 2',
+      name: 'Product 2',
+      brand:"XYZ",
+      availability:"In Stock",
       price: 20,
       category: 'Category 2',
       description: 'Description for Product 2',
-      image: 'product2.jpg'
+      imageUrl: 'product2.jpg'
     },
     // Add more products as needed
   ];
 
-  constructor() { }
+  constructor(private storeService: StoreService) { }
 
   ngOnInit(): void {
+    // this.getProducts();
+    console.log("Checking on Kisan Market111" +this.categoryType)
+
+    console.log("Checking on Kisan Service" +this.categoryType)
+    if (this.categoryType === "Agriculture Equipment" ){
+            this.getProductsBytype('Agri-Equip')
+            
+        }else if (this.categoryType === "Seeds" ){
+          this.getProductsBytype('Seeds')
+      }else if (this.categoryType === "Milk Business" ){
+        this.getProductsBytype('MilkService')
+    }else if (this.categoryType === "Pesticides" ){
+      this.getProductsBytype('Pesticides')
+}
+    
   }
 
+  getProducts(): void{
+    this.productSubscription = this.storeService.getAllProducts(this.count, this.sort)
+    .subscribe((_products)=> {
+      this.products = _products;
+    })
+  }
 
+  getProductsBytype(productType: string): void{
+    this.productSubscription = this.storeService.getProductsBytype(productType)
+    .subscribe((_products)=> {
+      this.products = _products;
+    })
+  }
 
+  refreshKisanMarket(){
+    this.ngOnInit();
+  }
 
   onAddToCart(): void{
     this.addToCart.emit(this.product);
--- src/app/pages/kisan-home/components/kisan-services/kisan-services.component.ts
+++ src/app/pages/kisan-home/components/kisan-services/kisan-services.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Product } from 'src/app/models/product.model';
+import { StoreService } from 'src/app/services/store.service';
 
 @Component({
   selector: 'app-kisan-services',
@@ -12,22 +15,38 @@ export class KisanServicesComponent implements OnInit {
   isMedicalS : boolean = false;
   isIrrigationS : boolean = false;
   isConstructionAS: boolean = false;
+  products: Array<Product> | undefined;
+  sort: string = 'desc';
+  count: string = '12';
+  productSubscription: Subscription | undefined;
   
 
-  constructor() { }
+  constructor(private storeService: StoreService) { }
 
   ngOnInit(): void {
+
+    // this.getServices();
 
     console.log("Checking on Kisan Service" +this.categoryType)
     if (this.categoryType === "Irrigation Services" ){
             this.isIrrigationS = true;
+            //we can call service here
+            this.getServicesBytype('Irrigation')
+            
         }else if (this.categoryType === "Medical Services" ){
           this.isMedicalS = true;
+          this.getServicesBytype('Medical')
       }else if (this.categoryType === "Electric Services" ){
         this.isElectricS = true;
+        this.getServicesBytype('Electric')
     }else if (this.categoryType === "Construction Assistance" ){
       this.isConstructionAS = true;
+      this.getServicesBytype('Construction')
 }
+
+
+
+
 
 console.log("Checking Value" +this.isElectricS)
 
@@ -40,6 +59,22 @@ console.log("Checking Value" +this.isElectricS)
     this.isConstructionAS = false
     this.ngOnInit();
 
+  }
+
+ 
+
+  getServices(): void{
+    this.productSubscription = this.storeService.getAllServices(this.count, this.sort)
+    .subscribe((_products)=> {
+      this.products = _products;
+    })
+  }
+
+  getServicesBytype(servicetype: string): void{
+    this.productSubscription = this.storeService.getServicesById(servicetype)
+    .subscribe((_products)=> {
+      this.products = _products;
+    })
   }
 
 }
